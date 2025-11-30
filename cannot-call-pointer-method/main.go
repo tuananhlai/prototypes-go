7f2ceda60abbb2689ac310e576ed169995f5de0c
Lai Tuan Anh <42168740+tuananhlai@users.noreply.github.com>
2025-11-30 16:43:50 +0900

    add cannot-call-pointer-method

--- cannot-call-pointer-method/main.go
+++ cannot-call-pointer-method/main.go
@@ -4,16 +4,34 @@ import "fmt"
 
 type duration int
 
+func newDuration(d int) duration {
+	return duration(d)
+}
+
 func (d *duration) Print() {
 	fmt.Println("duration is ", *d)
 }
 
-func main() {
-	d := duration(10)
-	d.Print()
+type greeter struct {
+	name string
+}
 
+func (g *greeter) Print() {
+	fmt.Println("greeting ", g.name)
+}
+
+func main() {
 	// Because 10 is a constant, it doesn't have a memory address, so
 	// trying to invoke a pointer receiver of `duration` will result in
 	// a compile error of `cannot call pointer method Print on duration`.
 	// duration(10).Print()
+	// (&newDuration(10)).Print()
+
+	// You can invoke pointer method once you assigned the duration value
+	// to a variable though.
+	d := duration(10)
+	d.Print()
+
+	// You can invoke a pointer method on the address of a struct literal though.
+	(&greeter{name: "John"}).Print()
 }

420e08c32971d084dba3e5cf8186ad94e3683513
Lai Tuan Anh <42168740+tuananhlai@users.noreply.github.com>
2025-12-19 08:39:42 +0900

    feat/simple-squirrel: small updates

--- simple-squirrel/main.go
+++ simple-squirrel/main.go
@@ -1,57 +1,27 @@
 package main
 
 import (
-	"log"
-	"time"
+	"fmt"
 
 	sq "github.com/Masterminds/squirrel"
-	"github.com/jmoiron/sqlx"
-	_ "github.com/lib/pq"
-)
-
-const (
-	connStr = "postgres://postgres:postgres@localhost:25432/pagila?sslmode=disable"
 )
 
 func main() {
-	db := sqlx.MustOpen("postgres", connStr)
-	qb := sq.StatementBuilder.PlaceholderFormat(sq.Dollar).RunWith(db)
+	qb := sq.StatementBuilder.PlaceholderFormat(sq.Dollar)
 
-	var res int
-	if err := qb.Select("1").QueryRow().Scan(&res); err != nil {
-		log.Fatalf("error: %v", err)
+	builders := map[string]ToSqler{
+		"select all":      qb.Select("*").From("actor"),
+		"select some":     qb.Select("first_name", "last_name").From("actor"),
+		"select filter":   qb.Select("title").From("file").Where("rental_rate = ?", 0.99),
+		"select distinct": qb.Select("distinct rating").From("film"),
 	}
 
-	selectAll(db, qb)
+	for name, builder := range builders {
+		fmt.Printf("%s: ", name)
+		fmt.Println(builder.ToSql())
+	}
 }
 
-func selectAll(db *sqlx.DB, qb sq.StatementBuilderType) {
-	query, _, err := qb.Select("actor_id", "first_name", "last_name", "last_update").From("actor").ToSql()
-	if err != nil {
-		log.Fatal(err)
-	}
-	rows, err := db.Queryx(query)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var actors []Actor
-	for rows.Next() {
-		var actor Actor
-		err = rows.StructScan(&actor)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		actors = append(actors, actor)
-	}
-
-	log.Println(len(actors), actors[:10])
-}
-
-type Actor struct {
-	ActorID    int       `db:"actor_id"`
-	FirstName  string    `db:"first_name"`
-	LastName   string    `db:"last_name"`
-	LastUpdate time.Time `db:"last_update"`
+type ToSqler interface {
+	ToSql() (string, []any, error)
 }

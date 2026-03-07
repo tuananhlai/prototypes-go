be40a0fa32a36e404d812cf19ccdca1658b499a9
tuananhlai <42168740+tuananhlai@users.noreply.github.com>
2026-03-07 02:31:05 +0000

    feat/benchmark: update file io test

--- cheatsheet-benchmark/file_io_test.go
+++ cheatsheet-benchmark/file_io_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 const (
-	testFilePath = "testdata/sample.txt"
+	testFilePath = "testdata/sample.dat"
 )
 
 // BenchmarkRawReadOneBytes benchmarks reading a file one byte at a time without buffer.
@@ -17,51 +17,61 @@ func BenchmarkRawReadOneBytes(b *testing.B) {
 		b.Fatalf("cannot open test file: %v\n", err)
 	}
 
+	buf := make([]byte, 1)
 	for b.Loop() {
-		_, err := unix.Seek(fd, 0, unix.SEEK_SET)
-		if err != nil {
-			b.Fatalf("cannot seek to the beginning of the file: %v", err)
-		}
-
-		buf := make([]byte, 1)
-		for {
-			n, err := unix.Read(fd, buf)
-			if err != nil {
-				b.Fatalf("cannot read from test file: %v", err)
-			}
-			if n == 0 {
-				break
-			}
-		}
+		_ = readFile(fd, buf)
 	}
 
 	_ = unix.Close(fd)
 }
 
-// BenchmarkRawReadMultipleBytes benchmarks reading a file multiple bytes at a time without a buffer.
-func BenchmarkRawReadMultipleBytes(b *testing.B) {
+// BenchmarkRawReadDiskAlignedBlockSize benchmarks reading a file multiple bytes at a time without a buffer.
+// The number of bytes read at once is a multiple of the disk block size.
+func BenchmarkRawReadDiskAlignedBlockSize(b *testing.B) {
 	fd, err := unix.Open(testFilePath, unix.O_RDONLY, 0644)
 	if err != nil {
 		b.Fatalf("cannot open test file: %v\n", err)
 	}
 
+	buf := make([]byte, 4096) // 4096 is a common disk block size
 	for b.Loop() {
-		_, err := unix.Seek(fd, 0, unix.SEEK_SET)
-		if err != nil {
-			b.Fatalf("cannot seek to the beginning of the file: %v", err)
-		}
-
-		buf := make([]byte, 1024)
-		for {
-			n, err := unix.Read(fd, buf)
-			if err != nil {
-				b.Fatalf("cannot read from test file: %v", err)
-			}
-			if n == 0 {
-				break
-			}
-		}
+		_ = readFile(fd, buf)
 	}
 
 	_ = unix.Close(fd)
+}
+
+// BenchmarkRawReadArbitraryBlockSize benchmarks reading a file multiple bytes at a time without a buffer.
+// The number of bytes read at once is NOT a multiple of the disk block size.
+func BenchmarkRawReadArbitraryBlockSize(b *testing.B) {
+	fd, err := unix.Open(testFilePath, unix.O_RDONLY, 0644)
+	if err != nil {
+		b.Fatalf("cannot open test file: %v\n", err)
+	}
+
+	buf := make([]byte, 4241)
+	for b.Loop() {
+		_ = readFile(fd, buf)
+	}
+
+	_ = unix.Close(fd)
+}
+
+func readFile(fd int, buf []byte) error {
+	_, err := unix.Seek(fd, 0, unix.SEEK_SET)
+	if err != nil {
+		return err
+	}
+
+	for {
+		n, err := unix.Read(fd, buf)
+		if err != nil {
+			return err
+		}
+		if n == 0 {
+			break
+		}
+	}
+
+	return nil
 }

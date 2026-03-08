01cd7f3755e14a9a851e1c2709b74a5f5c732ad9
tuananhlai <42168740+tuananhlai@users.noreply.github.com>
2026-03-08 02:46:09 +0000

    feat/benchmark: add writev

--- [todo] linux-deferred-write/main.go
+++ [todo] linux-deferred-write/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "golang.org/x/sys/unix"
+import (
+	"golang.org/x/sys/unix"
+)
 
 func main() {
-	unix.Open()
+	fd, err := unix.Open("test.txt", unix.O_CREAT|unix.O_WRONLY, 0644)
+	if err != nil {
+		panic(err)
+	}
+	buf1 := []byte("hello")
+	buf2 := []byte("world")
+	buf3 := []byte("linux")
+
+	_, err = unix.Writev(fd, [][]byte{buf1, buf2, buf3})
+	if err != nil {
+		panic(err)
+	}
 }
--- cheatsheet-benchmark/file_io_test.go
+++ cheatsheet-benchmark/file_io_test.go
@@ -7,57 +7,130 @@ import (
 )
 
 const (
-	testFilePath = "testdata/sample.dat"
+	readTargetFilePath = "testdata/sample.dat"
 )
 
-// BenchmarkRawReadOneBytes benchmarks reading a file one byte at a time without buffer.
+// BenchmarkRawReadOneBytes benchmarks reading a file one byte at a time.
 func BenchmarkRawReadOneBytes(b *testing.B) {
-	fd, err := unix.Open(testFilePath, unix.O_RDONLY, 0644)
+	fd, err := unix.Open(readTargetFilePath, unix.O_RDONLY, 0644)
 	if err != nil {
 		b.Fatalf("cannot open test file: %v\n", err)
 	}
 
 	buf := make([]byte, 1)
 	for b.Loop() {
-		_ = readFile(fd, buf)
+		_ = process(fd, buf)
 	}
 
 	_ = unix.Close(fd)
 }
 
-// BenchmarkRawReadDiskAlignedBlockSize benchmarks reading a file multiple bytes at a time without a buffer.
+// BenchmarkRawReadDiskAlignedBlockSize benchmarks reading a file multiple bytes at a time.
 // The number of bytes read at once is a multiple of the disk block size.
 func BenchmarkRawReadDiskAlignedBlockSize(b *testing.B) {
-	fd, err := unix.Open(testFilePath, unix.O_RDONLY, 0644)
+	fd, err := unix.Open(readTargetFilePath, unix.O_RDONLY, 0644)
 	if err != nil {
 		b.Fatalf("cannot open test file: %v\n", err)
 	}
 
 	buf := make([]byte, 4096) // 4096 is a common disk block size
 	for b.Loop() {
-		_ = readFile(fd, buf)
+		_ = process(fd, buf)
 	}
 
 	_ = unix.Close(fd)
 }
 
-// BenchmarkRawReadArbitraryBlockSize benchmarks reading a file multiple bytes at a time without a buffer.
+// BenchmarkRawReadArbitraryBlockSize benchmarks reading a file multiple bytes at a time.
 // The number of bytes read at once is NOT a multiple of the disk block size.
 func BenchmarkRawReadArbitraryBlockSize(b *testing.B) {
-	fd, err := unix.Open(testFilePath, unix.O_RDONLY, 0644)
+	fd, err := unix.Open(readTargetFilePath, unix.O_RDONLY, 0644)
 	if err != nil {
 		b.Fatalf("cannot open test file: %v\n", err)
 	}
 
 	buf := make([]byte, 4241)
 	for b.Loop() {
-		_ = readFile(fd, buf)
+		_ = process(fd, buf)
 	}
 
 	_ = unix.Close(fd)
 }
 
-func readFile(fd int, buf []byte) error {
+// BenchmarkWriteConcatBytes writes multiple byte arrays in a single `write` system call
+// by concatenating them together.
+func BenchmarkWriteConcatBytes(b *testing.B) {
+	filePath := "testdata/write_concat_bytes.dat"
+	fd, err := unix.Open(filePath, unix.O_CREAT|unix.O_WRONLY, 0644)
+	if err != nil {
+		b.Fatalf("cannot open target file: %v\n", err)
+	}
+	defer unix.Close(fd)
+
+	slices := make([][]byte, 4)
+	for i := range slices {
+		slices[i] = make([]byte, 256)
+	}
+
+	for b.Loop() {
+		var totalLen int
+		for _, s := range slices {
+			totalLen += len(s)
+		}
+
+		concatBuf := make([]byte, 0, totalLen)
+		for _, s := range slices {
+			concatBuf = append(concatBuf, s...)
+		}
+
+		_, err := unix.Write(fd, concatBuf)
+		if err != nil {
+			b.Fatalf("write failed: %v", err)
+		}
+
+		b.StopTimer()
+		err = unix.Truncate(filePath, 0)
+		if err != nil {
+			b.Fatalf("truncate failed: %v", err)
+		}
+		b.StartTimer()
+	}
+}
+
+// BenchmarkWriteWritev writes multiple byte arrays in a single `writev` system call, avoiding
+// the need to concatenate input byte arrays.
+func BenchmarkWriteWritev(b *testing.B) {
+	filePath := "testdata/write_writev.dat"
+
+	fd, err := unix.Open(filePath, unix.O_CREAT|unix.O_WRONLY, 0644)
+	if err != nil {
+		b.Fatalf("cannot open target file: %v\n", err)
+	}
+	defer unix.Close(fd)
+
+	slices := make([][]byte, 4)
+	for i := range slices {
+		slices[i] = make([]byte, 256)
+	}
+
+	for b.Loop() {
+		_, err := unix.Writev(fd, slices)
+		if err != nil {
+			b.Fatalf("writev failed: %v", err)
+		}
+
+		b.StopTimer()
+		err = unix.Truncate(filePath, 0)
+		if err != nil {
+			b.Fatalf("truncate failed: %v", err)
+		}
+		b.StartTimer()
+	}
+}
+
+// process reads the given file descriptor, then do something with it, while making use of
+// the given `buf` byte slice to store temporary read data.
+func process(fd int, buf []byte) error {
 	_, err := unix.Seek(fd, 0, unix.SEEK_SET)
 	if err != nil {
 		return err
@@ -71,6 +144,8 @@ func readFile(fd int, buf []byte) error {
 		if n == 0 {
 			break
 		}
+
+		// process the data inside `buf`
 	}
 
 	return nil

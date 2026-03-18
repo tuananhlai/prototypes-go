444ac17ccb70f275f03d3dd1cdbe816acb2ac049
Tuan Anh <42168740+tuananhlai@users.noreply.github.com>
2026-03-18 13:08:50 +0000

    feat/toy-telnet: add working version

---
+++ toy-telnet/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"net"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 3 {
+		panic("not enough arguments")
+	}
+
+	host, port := os.Args[1], os.Args[2]
+
+	conn, err := net.Dial("tcp4", fmt.Sprintf("%s:%s", host, port))
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Connection successful to %s, port %s\n", host, port)
+
+	bufrw := bufio.NewReadWriter(bufio.NewReader(conn), bufio.NewWriter(conn))
+
+	go func() {
+		rawRes := make([]byte, 4096)
+		for {
+			n, err := bufrw.Read(rawRes)
+			if err != nil {
+				if err != io.EOF {
+					fmt.Fprintf(os.Stderr, "error reading response: %v\n", err)
+				}
+				break
+			}
+			fmt.Println(string(rawRes[:n]))
+		}
+	}()
+
+	for {
+		fmt.Print("")
+		_, err := io.Copy(bufrw, os.Stdin)
+		if err != nil {
+			panic(err)
+		}
+	}
+}

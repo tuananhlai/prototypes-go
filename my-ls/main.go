7b9e1b9296cfb71a64d33642daa409465fc57c4f
Tuan Anh <42168740+tuananhlai@users.noreply.github.com>
2026-03-28 23:55:44 +0000

    feat/my-ls: init

---
+++ my-ls/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+
+	"golang.org/x/sys/unix"
+)
+
+func main() {
+	cwd, err := unix.Getwd()
+	if err != nil {
+		panic(err)
+	}
+
+	fd, err := unix.Open(cwd, unix.O_RDONLY, 0o644)
+	if err != nil {
+		panic(err)
+	}
+	defer unix.Close(fd)
+
+	buf := make([]byte, 4096)
+	for {
+		// Read the next directory entries from the cwd's file descriptor.
+		// The method tries to read as many **complete** directory entries into
+		// buf as possible before returning.
+		n, err := unix.ReadDirent(fd, buf)
+		if err != nil {
+			panic(err)
+		}
+		if n == 0 {
+			break
+		}
+
+		// Parse the raw bytes returned by `ReadDirent` into a slice of entry names.
+		_, _, names := unix.ParseDirent(buf[:n], -1, nil)
+		for _, name := range names {
+			fmt.Println(name)
+		}
+	}
+}

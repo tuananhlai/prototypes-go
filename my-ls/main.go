f69b32969a9cb5ecf899f213f7937f329f453afe
Tuan Anh <42168740+tuananhlai@users.noreply.github.com>
2026-03-30 06:17:19 +0000

    chore/my-ls: add -a flag

--- my-ls/main.go
+++ my-ls/main.go
@@ -1,23 +1,25 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"os"
+	"slices"
 
 	"golang.org/x/sys/unix"
 )
 
 func main() {
-	var wd string
-	var err error
-	if len(os.Args) == 1 {
-		wd, err = unix.Getwd()
-	} else {
-		wd = os.Args[1]
-	}
+	displayHiddenFiles := flag.Bool("a", false, "all")
 
-	if err != nil {
-		panic(err)
+	flag.Parse()
+
+	wd := flag.Arg(0)
+	var err error
+	if wd == "" {
+		wd, err = unix.Getwd()
+		if err != nil {
+			panic(err)
+		}
 	}
 
 	fd, err := unix.Open(wd, unix.O_RDONLY, 0o644)
@@ -25,6 +27,28 @@ func main() {
 		panic(err)
 	}
 	defer unix.Close(fd)
+
+	entryNames, err := readDir(fd)
+	if err != nil {
+		panic(err)
+	}
+
+	slices.Sort(entryNames)
+
+	for _, name := range entryNames {
+		if !*displayHiddenFiles && isHiddenFile(name) {
+			continue
+		}
+		fmt.Println(name)
+	}
+}
+
+func isHiddenFile(fileName string) bool {
+	return len(fileName) > 0 && fileName[0] == '.'
+}
+
+func readDir(fd int) ([]string, error) {
+	var retval []string
 
 	buf := make([]byte, 4096)
 	for {
@@ -35,7 +59,7 @@ func main() {
 		// Regular `Read` system call will return EISDIR
 		n, err := unix.ReadDirent(fd, buf)
 		if err != nil {
-			panic(err)
+			return nil, err
 		}
 		if n == 0 {
 			break
@@ -43,8 +67,8 @@ func main() {
 
 		// Parse the raw bytes returned by `ReadDirent` into a slice of entry names.
 		_, _, names := unix.ParseDirent(buf[:n], -1, nil)
-		for _, name := range names {
-			fmt.Println(name)
-		}
+		retval = slices.Concat(retval, names)
 	}
+
+	return retval, nil
 }
